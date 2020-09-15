0a16dbb4625b238003a8e6d703bf2bd6f5fdcffd
kouloughli Hemza <shoper_06@outlook.com>
2020-09-15 23:44:17 +0100

    UPDATED: Removed Spacing

--- src/Activity.php
+++ src/Activity.php
@@ -8,9 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     const UPDATED_AT = null;
-
     const CREATED_AT = 'activ_date';
-
+    
     protected $table = 'users_activity';
     protected $primaryKey = 'ref_activ';
 
